9340bf8ace0c1b691727da36ff31138168944477
profesorpizzolato-art <profesorpizzolato@gmail.com>
2026-04-06 17:19:51 -0300

    Update motor_calculos_avanzados.py

--- motor_calculos_avanzados.py
+++ motor_calculos_avanzados.py
@@ -1,12 +1,14 @@
 import numpy as np
 
-def calcular_fisica_perforacion(wob, rpm, torque, rop, depth, flow_rate):
+def calcular_fisica_perforacion(wob, rpm, torque, rop, profundidad, flow_rate):
     # Constantes Vaca Muerta
     diam_mecha = 8.5 
     area_mecha = np.pi * (diam_mecha**2) / 4
     
-    # 1. MSE (Energía Específica Mecánica)
-    mse = (wob / area_mecha) + (480 * torque * rpm) / (area_mecha * rop * 3.28)
+    # 1. MSE (Mechanical Specific Energy)
+    # Evitamos división por cero si ROP es 0
+    rop_efectiva = rop if rop > 0 else 0.1
+    mse = (wob / area_mecha) + (480 * torque * rpm) / (area_mecha * rop_efectiva * 3.28)
     mse_kpsi = mse / 1000
 
     # 2. Hidráulica (Velocidad Anular)
@@ -16,13 +18,13 @@ def calcular_fisica_perforacion(wob, rpm, torque, rop, depth, flow_rate):
     
     # 3. Control de Pozos (Kill Mud Weight)
     sidpp = 500
-    tvd = depth * 3.28
+    tvd = profundidad * 3.28 # Conversión a pies para la fórmula
     mw_actual = 11.5
     kmw = mw_actual + (sidpp / (0.052 * tvd))
 
     # 4. Carga en el Gancho (Hook Load)
-    bf = 0.82
-    peso_sarta = depth * 25
+    bf = 0.82 # Factor de flotación
+    peso_sarta = profundidad * 25 # lbs/pie aprox
     hook_load = (peso_sarta * bf) - (wob * 1000)
 
     return {
