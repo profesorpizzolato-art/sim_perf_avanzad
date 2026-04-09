e7adf0da823aa5bb3fe49adc3ffe4b22f10a8e6b
profesorpizzolato-art <profesorpizzolato@gmail.com>
2026-04-09 15:50:48 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-import streamlit as st
+ýimport streamlit as st
 import pandas as pd
 import numpy as np
 import time
@@ -642,10 +642,10 @@ if st.sidebar.button("🚨 ACTIVAR KICK (SURGENCIA)"):
 
 if st.session_state.get("tipo_evento") == "KICK":
     # La presión sube 5 psi por segundo si no cierran el BOP
-    if not pizarra["bop_cerrado"]:
+if not pizarra["bop_cerrado"]:
 pizarra["presion_base"] = pizarra.get("presion_base", 0) + 5
         st.error("⚠️ ¡AUMENTO DE PRESIÓN EN TUBING! ¡CERRAR BOP!")
-    else:
+else:
         st.success("✅ POZO CERRADO. CALCULAR KILL MUD WEIGHT.")
 
 # 2. Lógica de Pérdida de Circulación
